6834cec9bc51f6b9150f4aa1565c2f384b842fb3
Your Name <coughlin.nicholas@gmail.com>
2020-05-22 12:28:01 -0700

    api placeholder and action api request scaffolding

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = async () => {
+    // Bad Approach!!!
+    const response = await jsonPlaceholder.get('/posts');
+
     return {
-        type: 'FETCH_POSTS'
+        type: 'FETCH_POSTS',
+        payload: response
     };
 };
