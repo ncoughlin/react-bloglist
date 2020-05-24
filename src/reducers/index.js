a0dc11cfac2b9c9c7aa87b6572f9c384a938265e
Your Name <coughlin.nicholas@gmail.com>
2020-05-24 09:19:46 -0700

    API data is being fetched and has been mapped to state. Reducers complete.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
 
 export default combineReducers({
-    dummy: ()=> 'temp reducer'
+    posts: postsReducer
 });
